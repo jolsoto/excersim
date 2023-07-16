7a860b3b1c23647b881b999b889c030bac14b35a
Jose Luis Soto <jolsoto.dev@outlook.com>
2023-07-16 09:46:45 -0500

    Create leap.py

---
+++ Python/Bools/leap.py
@@ -0,0 +1,10 @@
+def leap_year(year):
+    if year%4==0:
+        result=True
+        if year%100==0:
+            result=False
+            if year%400==0:
+                result=True
+    else:
+        result=False
+    return result
