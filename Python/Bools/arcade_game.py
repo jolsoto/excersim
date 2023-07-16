a9bfb4918c59420b3e47ae9f59a74468e0a9a657
Jose Luis Soto <jolsoto.dev@outlook.com>
2023-07-16 10:10:18 -0500

    Update arcade_game.py
    instructions added

--- Python/Bools/arcade_game.py
+++ Python/Bools/arcade_game.py
@@ -1,3 +1,22 @@
+'''
+Task 1
+Define if Pac-Man eats a ghost
+Define the eat_ghost() function that takes two parameters (if Pac-Man has a power pellet active and if Pac-Man is touching a ghost) and returns a Boolean value if Pac-Man is able to eat the ghost. The function should return True only if Pac-Man has a power pellet active and is touching a ghost.
+
+Task 2
+Define if Pac-Man scores
+Define the score() function that takes two parameters (if Pac-Man is touching a power pellet and if Pac-Man is touching a dot) and returns a Boolean value if Pac-Man scored. The function should return True if Pac-Man is touching a power pellet or a dot.
+
+Task 3
+Define if Pac-Man loses
+Define the lose() function that takes two parameters (if Pac-Man has a power pellet active and if Pac-Man is touching a ghost) and returns a Boolean value if Pac-Man loses. The function should return True if Pac-Man is touching a ghost and does not have a power pellet active.
+
+Task 4
+Define if Pac-Man wins
+Define the win() function that takes three parameters (if Pac-Man has eaten all of the dots, if Pac-Man has a power pellet active, and if Pac-Man is touching a ghost) and returns a Boolean value if Pac-Man wins. The function should return True if Pac-Man has eaten all of the dots and has not lost based on the parameters defined in part 3.
+
+'''
+
 """Functions for implementing the rules of the classic arcade game Pac-Man."""
 def eat_ghost(power_pellet_active, touching_ghost):
     """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet.
