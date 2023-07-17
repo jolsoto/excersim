04a931ee97fd82844448f01b54be089e8437d2b4
Jose Luis Soto <jolsoto.dev@outlook.com>
2023-07-17 17:00:26 -0500

    Update matching_brackets.py

--- Python/Conditionals/matching_brackets.py
+++ Python/Conditionals/matching_brackets.py
@@ -2,3 +2,19 @@
 Instructions
 Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly. The string may also contain other characters, which for the purposes of this exercise should be ignored.
 '''
+def is_paired(input_string):
+    stack = []
+    opening = ["[", "{", "("]
+    closing = ["]", "}", ")"]
+    
+    for char in input_string:
+        if char in opening:
+            stack.append(char)
+        elif char in closing:
+            if len(stack) == 0:
+                return False
+            last_open = stack.pop()
+            if opening.index(last_open) != closing.index(char):
+                return False
+    
+    return len(stack) == 0
